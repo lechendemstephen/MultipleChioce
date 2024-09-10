429238047cfbe0b6e255ab665835f9d13f696d38
lechendemstephen <51188322+lechendemstephen@users.noreply.github.com>
2024-09-09 23:01:02 -0400

    creating all the models for course, question, answer and results

--- Exam/admin.py
+++ Exam/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Questions, Courses, Answer, Result
 # Register your models here.
+
+# admin management
+class CourseAdmin(admin.ModelAdmin): 
+    list_display = ('name', 'added_date')
+
+class QuestionAdmin(admin.ModelAdmin): 
+    list_display = ('course', 'question', 'created_date')
+
+class AnswerAdmin(admin.ModelAdmin): 
+    list_display = ('question', 'is_correct', 'answer_text', 'created_date')
+
+class ResultAdmin(admin.ModelAdmin): 
+    list_display = ('question', 'answer', 'total')
+
+
+
+
+admin.site.register(Courses, CourseAdmin)
+admin.site.register(Questions, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Result, ResultAdmin)
--- Exam/models.py
+++ Exam/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+class Courses(models.Model): 
+    name = models.CharField(max_length=30)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        verbose_name_plural = 'Courses'
+
+    def __str__(self): 
+        return self.name
+    
+    
+
+class Questions(models.Model): 
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    question = models.CharField(null=False, max_length=150)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta: 
+        verbose_name_plural = 'Questions'
+
+
+
+    def __str__(self): 
+        return self.question
+
+class Answer(models.Model): 
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=250, null=True)
+    is_correct = models.BooleanField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+
+    def __str__(self): 
+        return self.question.question
+
+class Result(models.Model): 
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    total = models.IntegerField()
+
+    result_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        verbose_name = 'Result'
+        verbose_name_plural = 'Results'
+
+    def __str__(self): 
+        return self.answer
+    
